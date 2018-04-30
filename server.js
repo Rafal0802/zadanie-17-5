a81524b8962bd6b8e8344b7ac60227376c350230
Rafal Swieton <rafal@eskrzynka.pl>
2018-05-01 01:30:47 +0200

    Add files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+
+app.use('/store', function(req, res, next) {
+	console.log('Jestem pośrednikiem przy żądaniu do /store');	
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.send('HELLO world!');
+});
+
+app.get('/store', function(req, res) {
+	res.send('To jest sklep');
+});
+
+app.listen(3000);
+
+app.use(function (req, res, next) {
+	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!');
+});
